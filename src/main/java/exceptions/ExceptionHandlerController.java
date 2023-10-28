ed31ee8c7670f71c716a3d86ec1594b12629489c
Cristaly Medeiros <crisgavnml@gmail.com>
2023-10-28 14:23:02 -0300

    Ajuste final

--- src/main/java/exceptions/ExceptionHandlerController.java
+++ src/main/java/exceptions/ExceptionHandlerController.java
@@ -13,7 +13,6 @@ public class ExceptionHandlerController {
     public ResponseEntity<String> handleCustomNotFoundException(CustomNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
-
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGenericException(Exception ex){
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
